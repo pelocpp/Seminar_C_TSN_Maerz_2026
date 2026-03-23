458e8e25b0a8e3e433c2bb83b70bdc652ca6ce84
Peter Loos <peter.loos@gmx.de>
2026-03-23 14:58:24 +0100

    Add project files.

---
+++ Getting_Started_C/MeineUnterprogramme.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+   // standard input / output
+
+
+void unterprogramm()
+{
+}
+
+// Unterprogramm
+void printHello ( int wieOft)
+{
+    // Python for-loop
+     //for i in range (0, 5, 1):
+     //   print("Hello Seminar")
+
+    // C for loop
+    for (int i = 0 ; i < wieOft ; i++ )
+    {
+        printf("Hello Seminar\n");
+    }
+}
---
+++ Getting_Started_C/Program.c
@@ -0,0 +1,69 @@
+#include <stdio.h>  
+
+extern void unterprogramm();
+
+extern void printHello(int wieOft);
+
+void main()
+{
+    int n;
+
+    printf("Bitte Wert eingeben: ");
+    scanf_s("%d", &n);
+
+    printHello ( 8 );
+}
+
+
+
+void main_Hello_World()
+{
+    printf("Hello World Seminar");
+
+    int n = 5;
+    int m;
+
+    // Anweisung:  Wertzuweisung  // assignment
+    // Von Rechts nach Links
+    m = n;   // in m ist eine KOPIE des Werts aus n
+}
+
+void main_if()
+{
+    int a = 6;
+    int b = 5;
+
+    unterprogramm();
+
+    if ( a < b ) 
+    {
+        printf("a kleiner b");
+        printf("noch was anderes");
+    }
+    else
+    {
+        printf("Bedingung nicht erfuellt");
+        printf("Bedingung nicht erfuellt");
+    }
+
+    printf("Fertig.");
+}
+
+void main_while()
+{
+    int n = 0;
+
+    while (n < 5) {
+        printf("C is beautiful\n");
+        n = n + 1;
+    }
+}
+
+
+// =========
+
+//
+//n = 123
+//m = 456
+//m = n
+//n = 100
